0f6412c91ee358963570637221869d22357bb78a
Rohan Sawai <sawairohan90@gmail.com>
2025-07-05 14:06:58 -0400

    Fix CSS loading issues by removing basePath for user site

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/rohansawai.github.io' : '',
 };
 
 export default nextConfig;
